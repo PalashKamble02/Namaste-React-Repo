0dac1625f31abc5f461432223968689755a802e1
newlearner2407 <manojbajpeyi804@gmail.com>
2024-05-01 14:14:44 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("h1",{},"Hello world From React");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+// const heading = React.createElement("h1",{},"Hello world From React");
